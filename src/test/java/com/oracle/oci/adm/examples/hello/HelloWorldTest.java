c3278a835abbf81923727b13313fa9c1169fc14a
douglas.clarke <douglas.clarke@oracle.com>
2023-05-08 15:07:45 -0400

    Update from zip

---
+++ src/test/java/com/oracle/oci/adm/examples/hello/HelloWorldTest.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2023, Oracle and/or its affiliates.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.oracle.oci.adm.examples.hello;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        HelloWorld.main(null);
+    }
+}
